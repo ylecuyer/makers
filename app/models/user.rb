6a5ce012503f1b17f311fd7e7608a98fbe5b1a92
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2018-10-16 21:58:29 +0200

    Add username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :todos
+
+  def color
+    require 'digest/md5'
+    Digest::MD5.hexdigest(username)[0..5]
+  end
 end
